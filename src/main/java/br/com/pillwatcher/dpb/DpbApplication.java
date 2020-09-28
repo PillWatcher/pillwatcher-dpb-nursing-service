ed0d9a506e16889ae14974784f516baefafb8d19
Filipe Dias <lipao.dias@hotmail.com>
2020-09-28 18:57:48 -0300

    :tada: Add initial structure of the Project

---
+++ src/main/java/br/com/pillwatcher/dpb/DpbApplication.java
@@ -0,0 +1,13 @@
+package br.com.pillwatcher.dpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pillwatcher/dpb/DpbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pillwatcher.dpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
